d0cfdcfdd80b4dfa54822ce629bb7294e931e3f6
YinniKun <dongrichard123@gmail.com>
2025-01-30 17:04:34 -0500

    modify for CLI application

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/schwartzlab-methods/spaghetti",  
     packages=find_packages(),  
+    entry_points={
+        "console_scripts": [
+            "spaghetti=src.cli_inference:main",
+        ],
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
---
+++ src/cli_inference.py
@@ -0,0 +1,45 @@
+'''
+Entry point for the CLI inference of SPAGHETTI.
+'''
+import os
+import argparse
+import inferences
+from PIL import Image
+
+def main(input, output, checkpoint):
+    '''
+    The main function for the CLI inference
+    args:
+        input: str, the input image or directory to translate
+        output: str, the output directory to save the translated image(s)
+        checkpoint: str, the path to the checkpoint file
+    '''
+    # check if input is a directory
+    if os.path.isdir(input):
+        # get all images
+        imgs = []
+        for path, _, files in os.walk(input):
+            for f in files:
+                if f.endswith((".jpg", ".jpeg", ".png", ".tiff", ".tif")):
+                    imgs.append(os.path.join(path, f))
+    else:
+        imgs = [input]
+    # create the model
+    model = inferences.Spaghetti(checkpoint)
+    # perform the inference
+    pil_imgs = [Image.open(img).convert("RGB") for img in imgs]
+    processed_imgs = model.pre_processing(pil_imgs, transform="default")
+    model.inference(processed_imgs, output)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="CLI for translating PCM images using SPAGHETTI")
+    parser.add_argument("--input", '-i', type=str, help="The input image or directory to translate")
+    parser.add_argument("--output", '-o', type=str, help="The output directory to save the translated image(s)")
+    parser.add_argument("--checkpoint", '-c', type=str, help="The path to the checkpoint file", required=True)
+    args = parser.parse_args()
+    assert os.path.exists(args.checkpoint), "The checkpoint file does not exist"
+    print("Welcome to SPAGHETTI CLI Inference")
+    print("Checkpoint file found at ", args.checkpoint, "will be used for inference")
+    print("Starting Inference...")
+    main(args.input, args.output, args.checkpoint)
+    print("Inference Completed。 Images saved to ", args.output)
