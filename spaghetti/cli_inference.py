fed0a6cc9ae0b39bfe11af4228ad63aa3aaeece3
YinniKun <dongrichard123@gmail.com>
2026-03-19 14:27:33 -0400

    update cli inference to support large dataset

--- spaghetti/cli_inference.py
+++ spaghetti/cli_inference.py
@@ -5,6 +5,7 @@ import os
 import argparse
 from spaghetti import inferences
 from PIL import Image
+from torch.utils.data import DataLoader, Dataset
 
 
 def inference(input, output, checkpoint):
@@ -30,10 +31,28 @@ def inference(input, output, checkpoint):
         names = [str(os.path.basename(input)).split(".")[0]]
     # create the model
     model = inferences.Spaghetti(checkpoint)
+    # create dataset
+    class ImageDataset(Dataset):
+        def __init__(self, img_paths, model):
+            self.img_paths = img_paths
+            self.model = model
+            # we need to perform the pre-processing on the images
+            # we will use the default transformation, but you can also define your own transformation using a callable
+            self.transform = "default"
+
+        def __len__(self):
+            return len(self.img_paths)
+
+        def __getitem__(self, idx):
+            img_path = self.img_paths[idx]
+            img = Image.open(img_path).convert("RGB")
+            transformed_img = self.model.pre_processing([img], transform=self.transform)[0]
+            return transformed_img
+    
     # perform the inference
-    pil_imgs = [Image.open(img).convert("RGB") for img in imgs]
-    processed_imgs = model.pre_processing(pil_imgs, transform="default")
-    model.inference(processed_imgs, names, output)
+    dataset = ImageDataset(imgs, model)
+    dataloader = DataLoader(dataset, batch_size=1, shuffle=False)
+    model.inference(dataloader, names, output)
 
 
 def main():
